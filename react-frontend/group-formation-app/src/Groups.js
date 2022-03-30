58b5b79d26f4ba488e6c82b3e44de63a07f92680
Nate <nmucha3@gatech.edu>
2022-03-29 22:44:46 -0400

    initial groups

--- react-frontend/group-formation-app/src/Groups.js
+++ react-frontend/group-formation-app/src/Groups.js
@@ -1,10 +1,21 @@
 import React from "react";
+import App from "./App";
 
 function Groups(){
     return(
-        <div>
-            <h1>Open Groups</h1>
+        <div className="App">
+            <h1 className="App-header">Open Groups
+            </h1>
+            <div className="App-group">
+                    group 1
+            </div>
+            <br></br>
+            <br></br>
+            <div className="App-group">
+                group 2
+            </div>
         </div>
+
     )
 }
 
