c366dfdcd64436095292243b50704eea3e1d00b1
Carson-Duerr <46726802+Carson-Duerr@users.noreply.github.com>
2022-03-29 20:45:11 -0400

    Created pages
    created courses, groups, group creation, profile creation, and students pages

---
+++ react-frontend/group-formation-app/src/CoursesHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CoursesHome(){
+    return(
+        <div>
+            <h1>Courses</h1>
+        </div>
+    )
+}
+
+export default CoursesHome;
---
+++ react-frontend/group-formation-app/src/GroupCreation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GroupCreation(){
+    return(
+        <div>
+            <h1>Group Creation</h1>
+        </div>
+    )
+}
+
+export default GroupCreation;
