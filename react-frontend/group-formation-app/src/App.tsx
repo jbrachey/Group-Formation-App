10f6aa4c5742138783b773f2cd8cda0ae2e842a6
Carson-Duerr <46726802+Carson-Duerr@users.noreply.github.com>
2022-03-29 21:15:02 -0400

    Routing

--- react-frontend/group-formation-app/src/App.tsx
+++ react-frontend/group-formation-app/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CoursesHome from './CoursesHome';
+import Groups from './Groups';
+import GroupCreation from './GroupCreation';
+import ProfileCreation from './ProfileCreation';
+import Students from './Students';
+import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={CoursesHome} />
+      <Route exact path="/groups" component={Groups} />
+      <Route exact path="/groupcreation" component={GroupCreation} />
+      <Route exact path="/profilecreation" component={ProfileCreation} />
+      <Route exact path="/students" component={Students} />
     </div>
   );
 }
