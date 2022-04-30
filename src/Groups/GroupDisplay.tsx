95fdc6d99a4d75992e81463a2098e4e15effa10f
robbiemcc7 <79654962+robbiemcc7@users.noreply.github.com>
2022-04-30 13:53:47 -0400

    Add files via upload
    new request stuff

--- src/Groups/GroupDisplay.tsx
+++ src/Groups/GroupDisplay.tsx
@@ -3,13 +3,31 @@ import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router';
 import ConciseScheduleView from '../Schedule/ConciseScheduleView';
 import GroupDisplayHeader from './GroupDisplayHeader';
-import { useState } from 'react';
+import { render } from '@testing-library/react';
+import React,{useState,useEffect} from 'react';
+import firebase from './../firebase.js';
 
-const GroupDisplay = ({groupID, name, availability, neededExp, numStudents, totalStudents}) => {
+const GroupDisplay = ({groupID, name, availability, neededExp, numStudents, totalStudents, requests}) => {
+    console.log("Display requests: ", requests);
     const navigate = useNavigate();
     const { user, courseID } = useParams();
-    const [requested, setRequested] = useState(false);
     const navigationURL = '/' + user + '/' + courseID + '/group/' + groupID;
+    const checkRequests = () => {
+        if (requests.includes(user)) {
+            return (<button className="groups-button">Request Pending</button>)
+        } else {
+            return (<button onClick={sendRequest} className="groups-button">Request to Join Team</button>)
+        }
+    }
+    const sendRequest = () => {
+        var currRequests = requests;
+        console.log("Success1")
+        currRequests.push(user)
+        firebase.db.collection("groups").doc(groupID).update({
+            requests: currRequests,
+        })
+        console.log("Success2")
+    }
     return (
         <div>
             <button onClick={() => {navigate(navigationURL)}} className="group-display">
@@ -18,11 +36,7 @@ const GroupDisplay = ({groupID, name, availability, neededExp, numStudents, tota
                 <br/>
                 <ConciseScheduleView availability={availability} />
                 <br/>
-                <button onClick={e => {
-                    e.stopPropagation();
-                    // handle request to join here
-                    setRequested(!requested);
-                    }} className="groups-button">{requested ? "Request Pending" : "Request to Join Team"}</button>
+                {checkRequests()}
             </button>
         </div>
     )
